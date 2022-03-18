bad87f6f10a21030ebd7c9dcebdccc40f6b7474f
VMFerents <vmferents@gmail.com>
2022-03-18 08:31:53 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import PostsList from "./component/PostsList";
 import MyButton from "./component/UI/Button/MyButton";
 import MyInput from "./component/UI/Input/MyInput";
 
 function App() {
-    let [posts, setPosts] = useState([
+    const [posts, setPosts] = useState([
         {id:1,title:'JavaScript',body:'Language of programming'},
         {id:2,title:'php',body:'Language of programming'},
         {id:3,title:'Python',body:'Language of programming'},
         {id:4,title:'C++',body:'Language of programming'},
         {id:5,title:'Ruby',body:'Language of programming'},
     ]);
+    const [title, setTitle] = useState('');
+    const bodyTitlePost = useRef();
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title)
+        console.log(bodyTitlePost.current.value)
+    }
+
   return (
       <>
           <form>
-              <MyInput type={'text'} placeholder={'название поста'}/>
-              <MyInput type={'text'} placeholder={'Описание поста'}/>
-              <MyButton>Добавить пост</MyButton>
+              {/*управляемый компонент.*/}
+              <MyInput type={'text'}
+                       value={title}
+                       onChange={e => setTitle(e.target.value)}
+                       placeholder={'название поста'}/>
+              {/*Неуправляемый компонент.*/}
+              <MyInput type={'text'}
+                       ref={bodyTitlePost}
+                       placeholder={'Описание поста'}/>
+              <MyButton onClick={addNewPost}>Добавить пост</MyButton>
           </form>
         <PostsList posts={posts}
                    title={'Список постов про языки програмирования'}/>
--- src/component/UI/Input/MyInput.jsx
+++ src/component/UI/Input/MyInput.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './MyInput.module.css';
 
-const MyInput = (props) => {
-    debugger
+const MyInput = React.forwardRef((props, ref) => {
     return (
-        <input {...props} className={styles.MyInput}/>
+        <input ref={ref} {...props} className={styles.MyInput}/>
     );
-};
+});
 
 export default MyInput;

6383d5320abef01a22325f617c311b1521d95e7e
VMFerents <vmferents@gmail.com>
2022-04-02 20:13:17 +0300

    useEffect Loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PostsList from "./component/PostsList";
 import PostForm from "./component/PostForm";
 import PostFilter from "./component/PostFilter";
 import MyModal from "./component/UI/MyModal/MyModal";
 import MyButton from "./component/UI/Button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./component/API/postService";
+import Loader from "./component/UI/Loader/Loader";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -17,6 +20,18 @@ function App() {
     const [filter,setFilter] = useState({sort:'',query:''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostLoading, setIsPostLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPosts()
+    },[] )
+
+    async function fetchPosts(){
+        setIsPostLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostLoading(false)
+    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -28,7 +43,7 @@ function App() {
 
   return (
       <div className={'App'}>
-
+          <button onClick={fetchPosts}>GET POST</button>
           <MyButton onClick={() => setModal(true)}>
               Добавить пост
           </MyButton>
@@ -39,9 +54,12 @@ function App() {
           <hr style={{margin: 15}} />
           <PostFilter filter={filter}
                       setFilter={setFilter}/>
-          <PostsList posts={sortedAndSearchedPosts}
-                     remove={removePost}
-                     title={'Список постов про языки програмирования'}/>
+          {isPostLoading
+              ? <Loader />
+              : <PostsList posts={sortedAndSearchedPosts}
+                           remove={removePost}
+                           title={'Список постов про языки програмирования'}/>
+          }
       </div>
   );
 }

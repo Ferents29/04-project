f10454fb2d5a9d8b7d6e297411a18a16f2668940
VMFerents <vmferents@gmail.com>
2022-04-17 17:36:11 +0300

    downloadable news feed, useObserver

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {usePosts} from "../hooks/usePosts";
 import {useFetching} from "../hooks/useFetching";
 import PostService from "../component/API/postService";
@@ -10,6 +10,7 @@ import PostFilter from "../component/PostFilter";
 import Loader from "../component/UI/Loader/Loader";
 import PostsList from "../component/PostsList";
 import Pagination from "../component/UI/Pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
 
 function Posts() {
     const [posts, setPosts] = useState([
@@ -25,17 +26,23 @@ function Posts() {
     const [totalPages, setTotalPages] = useState(0);
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
+    const lastElement = useRef()
+    const observer = useRef()
 
     const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
         const totalCount = response.headers['x-total-count'];
         setTotalPages(getPageCount(totalCount, limit))
     })
 
+    useObserver(lastElement, page < totalPages, isPostLoading, () => {
+        setPage(page + 1)
+    })
+
     useEffect(() => {
         fetchPosts(limit, page)
-    },[] )
+    },[page] )
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -66,11 +73,12 @@ function Posts() {
                         setFilter={setFilter}/>
             {postError && <h1>произошла ошыбка ${postError}</h1>}
             <div>
-                {isPostLoading
-                    ? <Loader />
-                    : <PostsList posts={sortedAndSearchedPosts}
-                                 remove={removePost}
-                                 title={'Список постов про языки програмирования'}/>
+                <PostsList posts={sortedAndSearchedPosts}
+                           remove={removePost}
+                           title={'Список постов про языки програмирования'}/>
+                <div ref={lastElement} style={{height:20, backgroundColor:'red',}}></div>
+                {isPostLoading &&
+                    <Loader />
                 }
                 <Pagination page={page}
                             changePage={changePage}
